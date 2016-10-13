f885430400869deb32681709f2d8a0fdb066d2a1
Gopal <gopalkildoliya@gmail.com>
2016-10-13 10:24:09 +0530

    Remove hover effect that changes date in input field of calendar as we hover over it

--- daterangepicker.js
+++ daterangepicker.js
@@ -1256,11 +1256,12 @@
             var cal = $(e.target).parents('.calendar');
             var date = cal.hasClass('left') ? this.leftCalendar.calendar[row][col] : this.rightCalendar.calendar[row][col];
 
-            if (this.endDate && !this.container.find('input[name=daterangepicker_start]').is(":focus")) {
+            // Remove hover effect that changes date in input field of calendar as we hover over it.
+            /*if (this.endDate && !this.container.find('input[name=daterangepicker_start]').is(":focus")) {
                 this.container.find('input[name=daterangepicker_start]').val(date.format(this.locale.format));
             } else if (!this.endDate && !this.container.find('input[name=daterangepicker_end]').is(":focus")) {
                 this.container.find('input[name=daterangepicker_end]').val(date.format(this.locale.format));
-            }
+            }*/
 
             //highlight the dates between the start date and the date being hovered as a potential end date
             var leftCalendar = this.leftCalendar;
